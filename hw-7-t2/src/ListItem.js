8ce26a21cb6a1dc7e32dfac9d23fceb574541745
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-17 02:35:39 +0300

    3

--- hw-7-t2/src/ListItem.js
+++ hw-7-t2/src/ListItem.js
@@ -1,4 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import "./App.css"
 
-const ListItem = ({ value }) => <li>{ value }</li>
-export default ListItem;
+export default function ListItem(props) {
+
+    const [crossed, setCrossed] = useState(true);
+    const [classes, setClasses] = useState('');
+    const [btnName, setBtnName] = useState('Выполнено');
+      
+    const handleClick = () => {
+        // {console.log(crossed, classes, btnName)}
+        if (!crossed) {
+            setClasses('');
+            setBtnName('Выполнено')
+        } else {
+            setClasses('crossedOut');
+            setBtnName('Вернуть')
+        }
+         setCrossed(!crossed);
+    }
+
+    return (
+        <div>
+            <li className={classes}>
+                { props.value }
+                {' '}
+                <button onClick={handleClick}>{btnName}</button>
+                {/* {console.log(crossed, classes, btnName)} */}
+            </li>
+        </div>
+    )
+}
